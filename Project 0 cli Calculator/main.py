363928ff2844986a2bb70fbcb0a6f347aca220af
Zarni Hlawn <zarnihlawn.dev@gmail.com>
2023-12-14 23:23:52 +0630

    .

--- Project 0 cli Calculator/main.py
+++ Project 0 cli Calculator/main.py
@@ -1,40 +1,102 @@
 # This is the cli version of the "Project 0 Calculator"
 
+from rich import print
+
 
 def main():
-    app = Calculator()
+    Calculator()
 
 
 class Calculator:
     def __init__(self):
-        print("Calculator Is Now Running Successfully ~")
+        print(f"[bold magenta]Calculator[/bold magenta] Is Now Running Successfully ~")
         while True:
             try:
-                self.choice = float(input("1. Simple 2. Scientific 3. Date  !Default is 'Simple'"))
+                self.choice = float(input(f"1. Simple 2. Scientific 3. Date  !Default is 'Simple'"))
                 break
             except ValueError:
-                print("Oops!  That was no valid number.  Try again...")
-        if self.choice == 1:
-            self.simple = Simple()
-        if self.choice == 2:
-            self.scientific = Scientific()
-        if self.choice == 3:
-            self.date = Date()
+                print(f"[red]Oops!  That was no valid number.  Try again...[/red]")
+        match self.choice:
+            case 1:
+                Simple()
+            case 2:
+                Converter()
+            case 3:
+                Date()
+            case _:
+                print(f"[red]Oops!  That was no valid options.  Try again...[/red]")
 
 
 class Simple:
     def __init__(self):
-        print("Simple Mode Activated")
+        self.mode = "Simple"
+        print(f"Simple Mode Activated")
+        while True:
+            try:
+                self.first_number = float(input(f"enter your first number: "))
+                break
+            except ValueError:
+                print(f"[red]Oops!  That was no valid number.  Try again...[/red]")
+        while True:
+            self.operator = str(input(f"enter operator: "))
+            if self.operator in operators:
+                break
+            else:
+                print(f"[red]Oops!  That was no valid operator.  Try again...[/red]")
+        while True:
+            try:
+                self.second_number = float(input(f"enter your second number: "))
+                break
+            except ValueError:
+                print(f"[red]Oops!  That was no valid number.  Try again...[/red]")
+        self.calculate()
+
+    def calculate(self):
+        result = None
+        match self.operator:
+            case '+':
+                result = self.first_number + self.second_number
+                print(f"[bold green]Result: {result}[/bold green]")
+            case '-':
+                result = self.first_number - self.second_number
+                print(f"[bold green]Result: {result}[/bold green]")
+            case '*':
+                result = self.first_number * self.second_number
+                print(f"[bold green]Result: {result}[/bold green]")
+            case '/':
+                if self.second_number != 0:
+                    result = self.first_number / self.second_number
+                    print(f"[bold green]Result: {result}[/bold green]")
+                else:
+                    print(f"Error: Division by zero.")
+            case '%':
+                if self.second_number != 0:
+                    result = self.first_number % self.second_number
+                    print(f"[bold green]Result: {result}[/bold green]")
+                else:
+                    print(f"Error: Division by zero.")
+            case _:
+                print(f"[red]Oops!  Unexpected Error.  Try again...[/red]")
 
 
-class Scientific:
+class Converter:
     def __init__(self):
-        print("Scientific Mode Activated")
+        print(f"Converter Mode Activated")
 
 
 class Date:
     def __init__(self):
-        print("Date Mode Activated")
+        self.result = None
+        print(f"Date Mode Activated")
+        self.first_date_time = int(input(f"First DateTime (DD/MM/YYYY) (HH/MM/SS): "))
+        self.second_date_time = int(input(f"Second DateTime (DD/MM/YYYY) (HH/MM/SS): "))
+        self.calculate()
+
+    def calculate(self):
+        self.result = (self.first_date_time - self.second_date_time)
+        self.result = self.result.__abs__()
 
 
-main()
+if __name__ == "__main__":
+    operators = ["+", "-", "*", "/", "%"]
+    main()
